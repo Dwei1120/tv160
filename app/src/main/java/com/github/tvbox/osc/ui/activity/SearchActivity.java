c3cdd77ca25af7c6865ab0be1f40d7dcf1c69fbc
Dwei1120 <110244356+Dwei1120@users.noreply.github.com>
2023-06-01 16:51:51 +0800

    Update SearchActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/SearchActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/SearchActivity.java
@@ -85,12 +85,11 @@ public class SearchActivity extends BaseActivity {
     private TvRecyclerView mGridView;
     private TvRecyclerView mGridViewWord;
     SourceViewModel sourceViewModel;
+    private RemoteDialog remoteDialog;
     private EditText etSearch;
     private TextView tvSearch;
     private TextView tvClear;
     private SearchKeyboard keyboard;
-    private TextView tvAddress;
-    private ImageView ivQRCode;
     private SearchAdapter searchAdapter;
     private PinyinAdapter wordAdapter;
     private String searchTitle = "";
@@ -171,8 +170,6 @@ public class SearchActivity extends BaseActivity {
         tvSearch = findViewById(R.id.tvSearch);
         tvSearchCheckboxBtn = findViewById(R.id.tvSearchCheckboxBtn);
         tvClear = findViewById(R.id.tvClear);
-        tvAddress = findViewById(R.id.tvAddress);
-        ivQRCode = findViewById(R.id.ivQRCode);
         mGridView = findViewById(R.id.mGridView);
         keyboard = findViewById(R.id.keyBoardRoot);
         mGridViewWord = findViewById(R.id.mGridViewWord);
@@ -269,7 +266,7 @@ public class SearchActivity extends BaseActivity {
                         loadRec(text);
                     }
                 } else if (pos == 1) {
-                    RemoteDialog remoteDialog = new RemoteDialog(mContext);
+                    remoteDialog = new RemoteDialog(mContext);
                     remoteDialog.show();
                 } else if (pos == 0) { //语音搜索
                     VoiceDialog.voicePermissions(mContext, new Handler.Callback() {
@@ -401,7 +398,6 @@ public class SearchActivity extends BaseActivity {
     }
 
     private void initData() {
-        refreshQRCode();
         initCheckedSourcesForSearch();
         Intent intent = getIntent();
         if (intent != null && intent.hasExtra("title")) {
@@ -440,12 +436,6 @@ public class SearchActivity extends BaseActivity {
 
     }
 
-    private void refreshQRCode() {
-        String address = ControlManager.get().getAddress(false);
-        tvAddress.setText(String.format("远程搜索使用手机/电脑扫描下面二维码或者直接浏览器访问地址\n%s", address));
-        ivQRCode.setImageBitmap(QRCodeGen.generateBitmap(address + "search.html", 300, 300));
-    }
-
     @Subscribe(threadMode = ThreadMode.MAIN)
     public void server(ServerEvent event) {
         if (event.type == ServerEvent.SERVER_SEARCH) {
@@ -477,7 +467,12 @@ public class SearchActivity extends BaseActivity {
 
     private void search(String title) {
         cancel();
+        if (remoteDialog != null) {
+            remoteDialog.dismiss();
+            remoteDialog = null;
+        }
         showLoading();
+        etSearch.setText(title);
         this.searchTitle = title;
         mGridView.setVisibility(View.INVISIBLE);
         searchAdapter.setNewData(new ArrayList<>());
